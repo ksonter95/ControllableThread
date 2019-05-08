e47ece664784f5fb2f5e4fc08cd0f2982f51e8c1
Kieran Sonter <ksonter95@gmail.com>
2019-05-08 08:05:20 +0200

    Returned to previous __init__.py

--- cthread/__init__.py
+++ cthread/__init__.py
@@ -1,14 +1,14 @@
 # Exceptions #
-from cthread import CThreadException
-from cthread import CallbackNotImplemented
-from cthread import InvalidArgument
-from cthread import InvalidState
-from cthread import InvalidMaxState
-from cthread import InvalidAlternativeState
-from cthread import InvalidCallback
-from cthread import InvalidName
-from cthread import InvalidQueue
+from cthread.cthread import CThreadException
+from cthread.cthread import CallbackNotImplemented
+from cthread.cthread import InvalidArgument
+from cthread.cthread import InvalidState
+from cthread.cthread import InvalidMaxState
+from cthread.cthread import InvalidAlternativeState
+from cthread.cthread import InvalidCallback
+from cthread.cthread import InvalidName
+from cthread.cthread import InvalidQueue
 
 # Classes #
-from cthread import ThreadState
-from cthread import ControllableThread
+from cthread.cthread import ThreadState
+from cthread.cthread import ControllableThread
