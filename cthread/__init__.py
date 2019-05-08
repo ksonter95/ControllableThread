966292b6f572a5d74076c0367ea9bc2d6516d71e
Kieran Sonter <ksonter95@gmail.com>
2019-05-08 07:58:56 +0200

    Added extra classes to __init__.py imports

--- cthread/__init__.py
+++ cthread/__init__.py
@@ -1,7 +1,11 @@
 # Exceptions #
 from cthread.cthread import CThreadException
+from cthread.cthread import CallbackNotImplemented
 from cthread.cthread import InvalidArgument
 from cthread.cthread import InvalidState
+from cthread.cthread import InvalidMaxState
+from cthread.cthread import InvalidAlternativeState
+from cthread.cthread import InvalidCallback
 from cthread.cthread import InvalidName
 from cthread.cthread import InvalidQueue
 
